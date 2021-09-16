a2cc9212c2015865808caae20106087dd2f1bbb7
Szewo <szewczyk.mat@yahoo.com>
2021-09-16 20:50:15 +0200

    all CRUD operations added

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,6 +6,7 @@ use App\Entity\Post;
 use App\Form\Type\PostType;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +19,7 @@ class IndexController extends AbstractController
     {
         $post = $postRepository->findAll();
 
-        return $this->render('index.html.twig', [
+        return $this->render('main/index.html.twig', [
             'all_posts' => $post
         ]);
     }
@@ -28,18 +29,25 @@ class IndexController extends AbstractController
      */
     public function addPost(Request $request)
     {
-
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if($form->isSubmitted() && $form->isValid())
+        {
             $post = $form->getData();
             $em = $this->getDoctrine()->getManager();
+            /** @var UploadedFile $file */
+            $file = $request->files->get('post')['image'];
+            if($file) {
+                $filename = md5(uniqid()) . '.' . $file->guessClientExtension();
+                $file->move($this->getParameter('images_directory'), $filename);
+                $post->setImage($filename);
+            }
             $em->persist($post);
             $em->flush();
 
-            return $this->redirectToRoute('app_index');
+            return $this->redirect('/');
         }
 
         return $this->render('post/add.html.twig', [
@@ -47,4 +55,50 @@ class IndexController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/edit-post/{id}", name="app_edit_post")
+     */
+    public function editPost(Request $request, int $id)
+    {
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            /** @var UploadedFile $file */
+            $file = $request->files->get('post')['image'];
+            if($file) {
+                $filename = md5(uniqid()) . '.' . $file->guessClientExtension();
+                $file->move($this->getParameter('images_directory'), $filename);
+                $post->setImage($filename);
+            }
+            $em->flush();
+
+            return $this->redirect('/');
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
+
+    /**
+     * @Route("/delete-post/{id}", name="app_delete_post")
+     */
+    public function deletePost(int $id, PostRepository $postRepository)
+    {
+
+        $post = $postRepository->findOneBy(['id' => $id]);
+        $pathToImage = $this->getParameter('images_directory') . '/' . $post->getImage();
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        unlink($pathToImage);
+        $em->flush();
+
+        return $this->redirect('/');
+
+    }
 }
--- src/Form/Type/PostType.php
+++ src/Form/Type/PostType.php
@@ -4,6 +4,7 @@ namespace App\Form\Type;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +16,10 @@ class PostType extends AbstractType
     {
         $builder->add('title', TextType::class)
             ->add('content', TextType::class)
+            ->add('image',FileType::class, [
+                'mapped' => false,
+                'required' => false
+            ])
             ->add('save', SubmitType::class);
     }
 
